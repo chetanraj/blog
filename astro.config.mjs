429d0dd40bebd4229a55da06a07124a66b928288
Chetan <chetan.narian@gmail.com>
2026-04-29 21:56:12 +0530

    feat: changed the framework from gatsby to astro 🚀

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://chetanraj.github.io/blog',
-  base: '/blog',
+  base: '/',
   integrations: [mdx()],
 });
