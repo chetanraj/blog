b18980fde6130e98be8b5cd35867315b9ac34341
Chetan <chetan.narian@gmail.com>
2026-04-30 07:37:37 +0530

    test: netlify build fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   base: '/',
+  output: 'server',
+  adapter: netlify(),
   integrations: [mdx(), tailwind()],
 });
